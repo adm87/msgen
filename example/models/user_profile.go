83259b88b933d7b6652a0727bea3332d6c0f0486
adm87 <aaron.d.malley@gmail.com>
2026-01-23 20:45:52 -0400

    analyzing spec to collect service info

---
+++ example/models/user_profile.go
@@ -0,0 +1,6 @@
+package models
+
+// UserProfile represents a user's profile information.
+type UserProfile struct {
+	UID string // Unique identifier for the user
+}
--- example/spec/spec.go
+++ example/spec/spec.go
@@ -1,4 +1,10 @@
 package spec
 
+import "github.com/adm87/msgen/example/models"
+
 type ExampleService interface {
+	// @Summary Get user profile by ID
+	// @Param userID string path true
+	// @Router /users/{userID} [get]
+	GetUserProfile(userID string) (models.UserProfile, error)
 }
